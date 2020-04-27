5dd9f2ec092aba1f7ee7d8c8e0f6646bab4c0391
birender veer singh <brarbirender001@gmail.com>
2020-04-27 16:51:19 -0400

    Functionality updated

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
-import requests 
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
+import requests 
+import json
+import time
 
+
+app = Flask(__name__)
+ask = Ask(app, "/")
 
 res = requests.get('https://www.worldometers.info/coronavirus/')
-# # Creation of bs4 object, which has all the methods
 soup = BeautifulSoup(res.text, "html.parser", parse_only=SoupStrainer(id="main_table_countries_today"))
+covid_data = []
+country = {}   
 
-#getiing all the data in covid_data
-covid_data = []   
-def get_all_table(url): 
-    table_body = url.find('tbody')
+def get_all_table(soup_object): 
+    table_body = soup_object.find('tbody')
     rows = table_body.find_all('tr')
+
+
     for row in rows:
         cols = row.find_all('td')
         covid_data.append([i.text.strip() for i in cols])
-    return covid_data    
-
-
-country = {}
-def get_country_data(table):
-    for each_country in table: 
+    for each_country in covid_data: 
         country_name = each_country[0]
         country_total_number = each_country[1]
         country[country_name] = country_total_number
-    print(country)    
+    print(country['India'])    
     return country
+@ask.launch
+def start_skill():
+    welcome_msg = render_template('welcome')
+    reprompt = render_template('reprompt')
+    return question(welcome_msg).reprompt(reprompt)
+
+# This messeage we need to edit 
+@ask.intent("YesIntent", convert={'Country': str})
+def share_data():
+    data = get_all_table(soup)
+    response = render_template("no_of_cases", country = Country, Data = data)
+    return statement(response)
+
+
+@ask.intent("NoIntent")
+def no_intent():
+    out = render_template('out_message')
+    return statement(out)
+
 if __name__ == '__main__':
-    get_all_table(soup)
-    get_country_data(covid_data)
+    app.run(debug=True)        
